e9ea45098c68e3674daf69bbac77fa858ee0e276
Delyan Angelov <delian66@gmail.com>
2023-08-10 17:55:25 +0300

    First public version

---
+++ README.md
@@ -0,0 +1,23 @@
+Purpose:
+===================
+This repo contains a small tool, that makes easier comparisons of the
+performance, between normally compiled V programs, and V programs compiled
+with PGO, using gcc or other C compilers, that support PGO.
+
+Usage:
+===================
+
+```bash
+cd /folder/to/your/V
+/path/to/repo/compare_normal_with_pgo.sh
+```
+
+You can parametrise it, by setting environment variables, for example:
+`V_OPTIONS='-gc none -prod' /path/to/repo/compare_normal_with_pgo.sh`
+
+See [compare_normal_with_pgo.sh](./compare_normal_with_pgo.sh) for
+a list of the supported env variables.
+
+Prerequisites:
+===================
+V, GCC, hyperfine
---
+++ compare_normal_with_pgo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## set -x
+
+alias xtime='/usr/bin/time -f "CPU: %Us\tReal: %es\tElapsed: %E\tRAM: %MKB\t%C"'
+function techo() { 
+	echo ">>> $(date '+%Y/%m/%d %H:%M:%S.%3N'): $1";
+}
+
+V="${V:-./v}"
+CC="${CC:-gcc}"
+V_OPTIONS="${V_OPTIONS:--prod}"
+VO='-keepc'
+PROGRAM="${PROGRAM:-examples/nbody.v}"
+PROGRAM_OPTIONS="${PROGRAM_OPTIONS:-}"
+HYPERFINE_OPTIONS="${HYPERFINE_OPTIONS:- --warmup=1 --shell=none}"
+
+techo "CC: $CC | VO: '$VO' | V_OPTIONS: '$V_OPTIONS' | PROGRAM: $PROGRAM | PROGRAM_OPTIONS: '$PROGRAM_OPTIONS' | HYPERFINE_OPTIONS: '$HYPERFINE_OPTIONS'"
+
+## ensure a clean experiment
+techo "Cleanup..."
+rm -rf profile_folder/
+$V wipe-cache
+
+techo "Compiling normal ..."
+xtime $V $VO $V_OPTIONS -cc $CC                                           -o normal           $PROGRAM
+
+techo "Compiling pgo_profile ..."
+xtime $V $VO $V_OPTIONS -cc $CC -cflags -fprofile-generate=profile_folder -o pgo_profile $PROGRAM
+./pgo_profile $PROGRAM_OPTIONS
+techo "Compiling pgo ..."
+xtime $V $VO $V_OPTIONS -cc $CC -cflags -fprofile-use=profile_folder      -o pgo         $PROGRAM
+
+ls -lart normal pgo_profile pgo
+
+techo "Measuring normal vs pgo, with hyperfine $HYPERFINE_OPTIONS"
+hyperfine $HYPERFINE_OPTIONS "./normal $PROGRAM_OPTIONS" "./pgo $PROGRAM_OPTIONS"
+hyperfine $HYPERFINE_OPTIONS "./normal $PROGRAM_OPTIONS" "./pgo $PROGRAM_OPTIONS"
+hyperfine $HYPERFINE_OPTIONS "./normal $PROGRAM_OPTIONS" "./pgo $PROGRAM_OPTIONS"
